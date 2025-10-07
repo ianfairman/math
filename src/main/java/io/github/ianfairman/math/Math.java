c325ab7c662cceb87e9ec45c4a7583868daf0f85
Ian Fairman <ian.fairman@gmail.com>
2025-10-07 11:28:28 +0100

    Initial commit.

---
+++ src/main/java/io/github/ianfairman/math/Math.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.ianfairman.math;
+
+/**
+ *
+ * @author Ian Fairman <ian.fairman@gmail.com>
+ */
+public class Math {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
