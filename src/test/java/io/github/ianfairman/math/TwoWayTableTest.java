4cee5ae57c50c961ffbe9df6da6c0e1e11420239
Ian Fairman <ian.fairman@gmail.com>
2025-10-14 11:45:32 +0100

    Constructor - no checking.

---
+++ src/test/java/io/github/ianfairman/math/TwoWayTableTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Ian Fairman <ian.fairman@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ianfairman.math;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Ian Fairman <ian.fairman@gmail.com>
+ */
+public class TwoWayTableTest {
+    
+    @Test
+    void shouldAcceptRectangularArray() {
+        // Given
+        double[][] array = {{2D, 3D, 4D}, {5D, 6D, 7D}};
+        
+        // When
+        try {
+            var _ = new TwoWayTable(array);
+        } catch (IllegalArgumentException _) {
+            fail();
+        }
+    }
+}
