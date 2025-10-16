645c5e993ff811d2927ad37631973fc2cf6c8fb6
Ian Fairman <ian.fairman@gmail.com>
2025-10-16 10:59:08 +0100

    statistics - minor refactorings.

--- src/main/java/io/github/ianfairman/math/Statistic.java
+++ src/main/java/io/github/ianfairman/math/Statistic.java
@@ -1,5 +1,7 @@
 package io.github.ianfairman.math;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 import static java.lang.System.arraycopy;
 import static java.util.Arrays.sort;
 
@@ -25,11 +27,11 @@ import static java.util.Arrays.sort;
  */
 public class Statistic {
     
-    public static double arithmeticMean(double[] input) {
+    public static double arithmeticMean(double... input) {
         return sum(input)/input.length;
     }
     
-    public static double sum(double[] input) {
+    public static double sum(double... input) {
         var sum = 0D;
         for (var i = 0; i < input.length; ++i) {
             sum += input[i];
@@ -37,8 +39,8 @@ public class Statistic {
         return sum;
     }
     
-    public static double[] deviation(double[] input) {
-        double[] result = new double[input.length];
+    public static double[] deviation(double... input) {
+        var result = new double[input.length];
         var average = arithmeticMean(input);
         for(var i = 0; i < input.length; ++i) {
             result[i] = input[i] - average;
@@ -46,37 +48,37 @@ public class Statistic {
         return result;
     }
 
-    public static double[] square(double[] input) {
-        double[] result = new double[input.length];
+    public static double[] square(double... input) {
+        var result = new double[input.length];
         for(var i = 0; i < input.length; ++i) {
-            result[i] = Math.pow(input[i], 2D);
+            result[i] = pow(input[i], 2D);
         }
         return result;
     }
 
-    public static double populationVariance(double[] input) {
+    public static double populationVariance(double... input) {
         return arithmeticMean(square(deviation(input)));
     }
 
-    public static double populationStandardDeviation(double[] input) {
-        return Math.sqrt(populationVariance(input));
+    public static double populationStandardDeviation(double... input) {
+        return sqrt(populationVariance(input));
     }
 
-    public static double sampleVariance(double[] input) {
+    public static double sampleVariance(double... input) {
         return sum(square(deviation(input)))/(input.length - 1);
     }
 
-    public static double sampleStandardDeviation(double[] input) {
-        return Math.sqrt(sampleVariance(input));
+    public static double sampleStandardDeviation(double... input) {
+        return sqrt(sampleVariance(input));
     }
 
-    public static double median(double[] input) {
-        double[] inputCopy = new double[input.length];
+    public static double median(double... input) {
+        var inputCopy = new double[input.length];
         arraycopy(input, 0, inputCopy, 0, input.length);
         sort(inputCopy);
-        int middleIndex = input.length / 2;
+        var middleIndex = input.length / 2;
         if (input.length % 2 == 0) {
-            return (inputCopy[middleIndex] + inputCopy[middleIndex - 1]) / 2;
+            return arithmeticMean(inputCopy[middleIndex], inputCopy[middleIndex - 1]);
         }   
         return inputCopy[middleIndex];
     }
